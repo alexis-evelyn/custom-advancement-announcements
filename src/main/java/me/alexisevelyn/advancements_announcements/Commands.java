3088aacf8263cd2aaeca378c1c5dc3f4ef8cee70
Alex Evelyn <gpg@senorcontento.com>
2020-02-12 23:51:44 -0500

    Cleaned Up Some Code

--- src/main/java/me/alexisevelyn/advancements_announcements/Commands.java
+++ src/main/java/me/alexisevelyn/advancements_announcements/Commands.java
@@ -4,9 +4,11 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import org.bukkit.ChatColor;
+
 public class Commands implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		sender.sendMessage("Hello World!!!");
+		sender.sendMessage(ChatColor.GOLD + "Hello World!!!");
 		return true;
 	}
 }
--- src/main/java/me/alexisevelyn/advancements_announcements/Main.java
+++ src/main/java/me/alexisevelyn/advancements_announcements/Main.java
@@ -1,18 +1,41 @@
 package me.alexisevelyn.advancements_announcements;
 
+// Bukkit Imports
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.advancement.Advancement;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerAdvancementDoneEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+// Third Party Imports
 import me.clip.placeholderapi.PlaceholderAPI;
-import net.md_5.bungee.api.ChatColor;
 
 public class Main extends JavaPlugin implements Listener {
 	private boolean debug = true;
 	private boolean essentials = false;
+	
+	// Getters and Setters
+	
+	public boolean isDebug() {
+		return this.debug;
+	}
+
+	public void setDebug(boolean debug) {
+		this.debug = debug;
+	}
+	
+	public boolean isEssentials() {
+		return this.essentials;
+	}
+
+	public void setEssentials(boolean essentials) {
+		this.essentials = essentials;
+	}
+	
+	// Event Listeners
 	
 	@Override
     public void onEnable() {
@@ -28,7 +51,7 @@ public class Main extends JavaPlugin implements Listener {
 		
 		if (Bukkit.getPluginManager().getPlugin("Essentials") != null) {
 			getLogger().info("Essentials Found!!! Can Use Essentials' Placeholders!!!");
-			this.essentials = true;
+			this.setEssentials(true);
         } else {
             getLogger().info("Essentials Not Found!!! Cannot use Essentials Placeholder!!!");
         }
@@ -61,12 +84,14 @@ public class Main extends JavaPlugin implements Listener {
 			return;
 		}
 		
-		if(this.debug) {
+		if(isDebug()) {
 			getLogger().info(ChatColor.GREEN + "Debug");
 			getLogger().info(ChatColor.GREEN + "---");
+			
 			for(String value : advancement.getCriteria()) { 
 				getLogger().info(ChatColor.GREEN + "Key/Value: " + value);
 			}
+			
 			getLogger().info(ChatColor.GREEN + "---");
 			
 			getLogger().info(ChatColor.GREEN + "Advancement Made: " + advancementMessage);
